0e8d2573cb5980ef58de92dfcbe36ec2f3148741
vikassinghrathor <rathorevikas2809@gmail.com>
2024-12-27 00:10:32 +0530

    fix: error

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
-/// <reference types="vitest" />
-import legacy from '@vitejs/plugin-legacy';
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), legacy()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
+  plugins: [react()],
+  build: {
+    outDir: 'build', // Change to 'build' if Netlify or your host requires this folder
   },
 });
